b99a8f0805c2bd9190d6969eb633e4dd20ea5666
loreabad6 <lore.abad6@gmail.com>
2020-02-19 23:40:08 +0100

    shiny done

--- shinyApp.R
+++ shinyApp.R
@@ -1,21 +1,31 @@
 library(tmap)
+library(sf)
 
 if (require("shiny")) {
   
   result = st_read('data/result.gpkg')
-  # data(World)
   result_vars = setdiff(names(result), c("id", "name", "geom"))
   
   
   ui <- fluidPage(
-    tmapOutput("map"),
-    selectInput("attribute", label = "Select variable: ", result_vars)
+    # Application title
+    titlePanel("Salzburg District population and touristic attractions"),
+    
+    # Top panel with municipality name
+    verticalLayout(
+      
+      wellPanel(textOutput("municipality")),
+      
+      # the map itself
+      tmapOutput("map"),
+      selectInput("attribute", label = "Select variable: ", result_vars)
+    )
   )
   
   server <- function(input, output, session) {
     output$map <- renderTmap({
       tm_shape(result) +
-        tm_polygons(col = result_vars[1], zindex = 401, style = 'jenks')
+        tm_polygons(col = result_vars[1], zindex = 401, style = 'jenks', palette = 'viridis')
     })
     
     observe({
@@ -23,24 +33,17 @@ if (require("shiny")) {
       tmapProxy("map", session, {
         tm_remove_layer(401) +
           tm_shape(result) +
-          tm_polygons(var, zindex = 401, style = 'jenks')
+          tm_polygons(var, zindex = 401, style = 'jenks', palette = 'viridis')
       })
     })
     
-    observeEvent(input$map_shape_click, {
-      
-      #create object for clicked polygon
-      click <- input$map_shape_click
-      
-      castles %>% filter(GEMNR == click$id)
-      
-      print(click$id)
-      
+    observe({
+      event <- input$map_shape_click
+      output$municipality <- renderText(as.character(result$name[paste0('X',result$id) == event$id]))
+
     })
     
-    # output$table <- renderTable({
-    #   df()
-    # })
+
   }	
   
   
