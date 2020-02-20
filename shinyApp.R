4bae2907d813150a7bade38b8f9f56ef459550d1
loreabad6 <lore.abad6@gmail.com>
2020-02-20 07:18:14 +0100

    added comments to scripts

--- shinyApp.R
+++ shinyApp.R
@@ -3,18 +3,20 @@ library(sf)
 
 if (require("shiny")) {
   
+  ## Load the result just created 
   result = st_read('data/result.gpkg')
   ## Remove variables that will not be options for plotting
   result_vars = setdiff(names(result), c("id", "name", "geom"))
   
-  
+  # Prepare the basic layout of the User Interface
   ui <- fluidPage(
     # Application title
     titlePanel("Salzburg District population and touristic attractions"),
     
-    # Top panel with municipality name
+    # Include a vertical layout to add bith the text box and the map
     verticalLayout(
       
+      # Top panel with municipality name
       wellPanel(textOutput("municipality")),
       
       # the map itself
@@ -23,12 +25,25 @@ if (require("shiny")) {
     )
   )
   
+  # Prepare the server 
   server <- function(input, output, session) {
+    
+    # Here comes the code for the tmap (remember the interactive version?)
     output$map <- renderTmap({
       tm_shape(result) +
-        tm_polygons(col = result_vars[1], zindex = 401, style = 'jenks', palette = 'viridis')
+        tm_polygons(
+          # To plot the first variable 
+          col = result_vars[1],
+          # This sets the layer number for the municipalities to plot. 
+          # Always set as 400 + n
+          zindex = 401, 
+          style = 'jenks', 
+          palette = 'viridis'
+        )
     })
     
+    # Adding some interaction, this section will change the attribute 
+    # plotted when the user selects it on the drop-down list
     observe({
       var <- input$attribute
       tmapProxy("map", session, {
@@ -38,16 +53,24 @@ if (require("shiny")) {
       })
     })
     
+    # This will display the name of the municipality on the 
+    # text box on top of the map when its polygon is clicked on the map
     observe({
       event <- input$map_shape_click
-      output$municipality <- renderText(as.character(result$name[paste0('X',result$id) == event$id]))
-
+      output$municipality <- renderText(
+        # We had to do some workarounds for this section to work properly,
+        # due to data classes mistmatches 
+        # Essentially we obtain the name of the municipality that corresponds 
+        # to the id clicked on the map by matching it to the municipality id 
+        as.character(result$name[paste0('X',result$id) == event$id])
+      )
+      
     })
     
-
+    
   }	
   
-  
+  # Save the app
   app <- shinyApp(ui, server)
   if (interactive()) app
 }
